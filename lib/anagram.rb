536237b7d4cdef7fc0af1928cc263bb3403a6a3d
Pete Fowler <fowler.pete@gmail.com>
2022-09-19 11:41:48 -0600

    commit tree 7428eb91c856655cee579fa5bf617bf795a846be

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,24 @@
-# Your code goes here!
+require 'pry'
+
+class Anagram
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(list)
+    matches = []
+    arr = @word.chars.sort
+   
+    list.each do |word| 
+      arr2 = word.chars.sort
+      if arr == arr2
+        matches.push word
+      end
+    end
+    puts matches
+    matches
+    
+
+  end 
+end
